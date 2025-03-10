b3e2942e071bb6ab44a57f464b2f253506f03e7f
Andrew Payne <andy.payne@mcneel.com>
2025-03-10 12:07:27 -0400

    initial configuration to get rhino.compute working in Rhino 9

--- src/compute.geometry/Program.cs
+++ src/compute.geometry/Program.cs
@@ -25,7 +25,7 @@ namespace compute.geometry
             Logging.Init();
 
             RhinoInside.Resolver.Initialize();
-            RhinoInside.Resolver.UseLatest = false;
+            RhinoInside.Resolver.UseLatest = true;
 #if DEBUG
             // Uncomment the following to debug with core Rhino source. This
             // tells compute to use a different RhinoCore than what RhinoInside thinks
